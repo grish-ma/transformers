c0b9f117d2ac029d9108d8c8a3ac1b7fdfd0dddb
grish-ma <144985869+grish-ma@users.noreply.github.com>
2024-11-04 00:03:20 -0800

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -27,7 +27,7 @@ from packaging import version
 from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
-from ...activations import ACT2FN
+from transformers.src.transformers.activations import ACT2FN
 from ...generation import GenerationMixin
 from ...modeling_attn_mask_utils import _prepare_4d_attention_mask_for_sdpa, _prepare_4d_causal_attention_mask_for_sdpa
 from ...modeling_outputs import (
