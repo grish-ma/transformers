b6479d1fc70424821604b653f1f04af844f709d8
grish-ma <144985869+grish-ma@users.noreply.github.com>
2024-11-15 02:53:34 -0800

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -29,6 +29,7 @@ from torch import nn
 from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 print("Second set of imports done") 
 
+print("starting ACT2FN import") 
 from transformers.activations_tf import ACT2FN
 print("transformers.activations_tf import ACT2FN") 
 
