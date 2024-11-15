eb828b6b52794b841e7ba7253b0c5a023b9c478f
grish-ma <144985869+grish-ma@users.noreply.github.com>
2024-11-15 03:02:38 -0800

    Update modeling_gpt2.py

--- src/transformers/models/gpt2/modeling_gpt2.py
+++ src/transformers/models/gpt2/modeling_gpt2.py
@@ -43,7 +43,9 @@ from transformers.modeling_outputs import (
 )
 print("transformers.modeling_outputs")
 from transformers.modeling_utils import PreTrainedModel, SequenceSummary
+print("MODELING_utils")
 from transformers.pytorch_utils import Conv1D, find_pruneable_heads_and_indices, prune_conv1d_layer
+print("PYTORCH.utils")
 
 print("BEFORE transformers.utils import()")
 from transformers.utils import (
